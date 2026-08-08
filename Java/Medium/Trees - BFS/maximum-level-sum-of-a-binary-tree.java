11889a25c3b0330e6eacda7aeeb7c004507d7d68
Dayalan D S <85922079+daya1an@users.noreply.github.com>
2026-08-09 01:46:41 +0530

    Add maximum-level-sum-of-a-binary-tree
    Implement a solution to find the maximum level sum of a binary tree using BFS.

---
+++ Java/Medium/Trees - BFS/maximum-level-sum-of-a-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        
+        List<Integer> levelSums = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+
+            int size = q.size();
+            int sum = 0;
+
+            for (int i = 0; i < size; i++){
+
+                TreeNode node = q.poll();
+
+                sum += node.val;
+                if (i == size-1)
+                    levelSums.add(sum);
+                
+                if(node.left != null)
+                    q.offer(node.left);
+                
+                if(node.right != null)
+                    q.offer(node.right);
+            }
+        }
+        return levelSums.indexOf(Collections.max(levelSums))+1;
+    }
+}
