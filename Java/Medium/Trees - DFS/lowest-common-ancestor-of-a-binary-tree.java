15beb2cb989aabfcfc058aaf2cf11d6bb4640a7e
Dayalan D S <85922079+daya1an@users.noreply.github.com>
2026-08-08 23:45:45 +0530

    adding lowest-common-ancestor-of-a-binary-tree

---
+++ Java/Medium/Trees - DFS/lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || p == null || q == null) {
+            return null;
+        } 
+
+        if((p.left == q) || (p.right == q)) {
+            return p;
+        }
+
+        if((q.left == p) || (q.right == p)) {
+            return q;
+        }
+
+        return dfs(root, p, q);   
+    }
+
+    private TreeNode dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return null;
+        }
+
+        if(root == p || root == q) {
+            return root;
+        }
+        TreeNode left = dfs(root.left, p, q);
+        TreeNode right = dfs(root.right, p, q);
+
+        if(left != null && right != null) {
+            return root;
+        }
+        if(left != null) {
+            return left;
+        }
+        return right;
+    }
+}
